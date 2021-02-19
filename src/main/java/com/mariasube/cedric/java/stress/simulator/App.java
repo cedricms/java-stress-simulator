5599f58887c396e464146790f195cc5e3e4cc861
Cédric Maria-Sube <cedric.mariasube@viseo.com>
2021-02-19 18:36:55 +0100

    Adds the default structure to the project.

---
+++ src/main/java/com/mariasube/cedric/java/stress/simulator/App.java
@@ -0,0 +1,13 @@
+package com.mariasube.cedric.java.stress.simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
